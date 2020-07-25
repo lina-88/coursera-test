e8eb3e1302a278ba1fc75d21def7035a5f58cfd7
Lina <lina.walid8@gmail.com>
2020-07-25 15:08:33 +0200

    folders

---
+++ module4/js/myscript.js
@@ -0,0 +1,20 @@
+/*var message = "global";
+function a () {
+    var message="index a";
+    console.log(message);
+   
+}
+a();*/
+/*var obj=new Object();
+obj.name="facebook";
+obj.ceo=new Object();
+obj.ceo.firstname="mark";
+console.log(obj);*/
+var facebook={
+    name:"face",
+    ceo:{
+        firstname:"mark"
+    },
+    "ay haga":"ayhaga"
+};
+console.log(facebook);
